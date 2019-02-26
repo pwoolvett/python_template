f0b2f62b663c12ad5c233e4db0f139289a2a55b7
Pablo <pablowoolvett@gmail.com>
2019-02-26 18:52:22 -0300

    python hook

---
+++ hooks/post_gen_project.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+from shutil import copy2 as copy
+from subprocess import run 
+
+def exec(string:str):
+    return run([*string.split()])
+
+# create dotenv file
+copy('.env.dist','.env')
+
+with open('.env','a+') as dotenv:
+    dotenv.write('FILE_CREATED_BY=python_template\n')
+
+# initialize git repo
+commands = [
+    "git init",
+    "git remote add origin {{ cookiecutter.repo }}",
+    "git add .",
+    'git commit -m "initial commit"',
+    "git push --set-upstream origin master",
+]
+for command in commands:
+    exec(command)
+
+# install virtualenv into ./.venv/ and run poetry install
+exec('tox -e venv')
+
