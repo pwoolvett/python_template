3b01677191da58ba41751e34282633caf2a11970
Pablo <pablowoolvett@gmail.com>
2019-02-26 18:42:06 -0300

    echo hook

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,18 +1,21 @@
 
 #!/usr/bin/env bash
-# create dotenv file
-cp .env.dist .env
-echo -e "FILE_CREATED_BY=python_template\n">> .env
+
+echo $PWD
+
+# # create dotenv file
+# cp .env.dist .env
+# echo -e "FILE_CREATED_BY=python_template\n">> .env
 
 
-# initialize git repo
-git init
-git remote add origin {{ cookiecutter.repo }}
-git add .
-git commit -m "initial commit"
-git push --set-upstream origin master
+# # initialize git repo
+# git init
+# git remote add origin {{ cookiecutter.repo }}
+# git add .
+# git commit -m "initial commit"
+# git push --set-upstream origin master
 
 
-# install virtualenv into ./.venv/ and run poetry install
-tox -e venv
+# # install virtualenv into ./.venv/ and run poetry install
+# tox -e venv
 
