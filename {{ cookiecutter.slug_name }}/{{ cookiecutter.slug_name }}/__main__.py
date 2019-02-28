58d3a4ee4de90d7c3e96622c3ded12717c3fb540
Pablo <pablowoolvett@gmail.com>
2019-02-28 09:34:25 -0300

    added poetry script install

--- {{ cookiecutter.slug_name }}/{{ cookiecutter.slug_name }}/__main__.py
+++ {{ cookiecutter.slug_name }}/{{ cookiecutter.slug_name }}/__main__.py
@@ -1,9 +1,15 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 """Expose scripts"""
+import sys as _sys
 
-from fire import Fire as _Fire
+from fire import Fire as _Fire_
 
 from {{ cookiecutter.slug_name }}.scripts import *  # noqa: E123
 
-_Fire()
+if __name__ == "__main__":
+    _Fire_(_sys.modules[__name__])
+else:
+    def _Fire(*a, **kw):
+        return _Fire_(_sys.modules[__name__])
+
